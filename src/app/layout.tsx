0d96bf33cce3fc709a1b9de46c08b407347bdbcb
墨綠BG <s5460703@gmail.com>
2024-05-02 13:35:53 +0800

    ✨ feat：设置 layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
-import '@antv/larkmap/*.less'
 
 import '@/common/styles/globals.scss';
 
