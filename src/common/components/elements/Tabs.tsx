96a4443fb7f596ba7cf1d1e45b489edc8011f6f7
墨綠BG <s5460703@gmail.com>
2024-05-02 21:27:52 +0800

    ✨ feat：新增 Hover 动画

--- src/common/components/elements/Tabs.tsx
+++ src/common/components/elements/Tabs.tsx
@@ -9,12 +9,64 @@ interface TabProps {
   children: React.ReactNode;
 }
 
+interface TabsItemProps {
+  tab: TabProps;
+  tabs: TabProps[];
+  index: number;
+  activeTab: number;
+  handleTabClick: (index: number) => void;
+}
+
 interface TabsProps {
   tabs: TabProps[];
 }
 
 export const Tab = ({ children }: TabProps) => {
   return <>{children}</>;
+};
+
+const TabItem: React.FC<TabsItemProps> = ({
+  tab,
+  index,
+  tabs,
+  activeTab,
+  handleTabClick,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  return (
+    <button
+      key={index}
+      className={`px-4 py-2 text-center font-medium ${tab.labelIcon && `flex items-center gap-1`} ${
+        activeTab === index
+          ? 'bg-neutral-500 text-neutral-100 dark:bg-neutral-400 dark:text-neutral-900'
+          : 'bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400 hover:dark:text-neutral-200'
+      } ${
+        index === 0
+          ? 'md:rounded-tl'
+          : index === tabs.length - 1
+            ? 'md:rounded-tr'
+            : ''
+      }`}
+      onClick={() => handleTabClick(index)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={`${activeTab === index && 'animate-pulse'} ${isHovered && '-rotate-12'}`}
+      >
+        {tab.labelIcon}
+      </div>
+      {tab.label}
+    </button>
+  );
 };
 
 export const Tabs = ({ tabs }: TabsProps) => {
@@ -28,26 +80,14 @@ export const Tabs = ({ tabs }: TabsProps) => {
     <div>
       <div className="flex flex-col gap-1.5 sm:flex-row md:gap-1">
         {tabs.map((tab, index) => (
-          <button
+          <TabItem
             key={index}
-            className={`px-4 py-2 text-center font-medium ${tab.labelIcon && `flex items-center gap-1`} ${
-              activeTab === index
-                ? 'bg-neutral-500 text-neutral-100 dark:bg-neutral-400 dark:text-neutral-900'
-                : 'bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-400 hover:dark:text-neutral-200'
-            } ${
-              index === 0
-                ? 'md:rounded-tl'
-                : index === tabs.length - 1
-                  ? 'md:rounded-tr'
-                  : ''
-            }`}
-            onClick={() => handleTabClick(index)}
-          >
-            <div className={`${activeTab === index && 'animate-pulse'}`}>
-              {tab.labelIcon}
-            </div>
-            {tab.label}
-          </button>
+            tab={tab}
+            index={index}
+            tabs={tabs}
+            activeTab={activeTab}
+            handleTabClick={handleTabClick}
+          />
         ))}
       </div>
       <div className="rounded border px-4 py-8 dark:border-neutral-800 sm:px-8">
