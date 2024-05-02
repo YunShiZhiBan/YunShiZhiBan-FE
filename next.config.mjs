a5a595f31ca6cacfbcf866189b8f6406a6378f85
墨綠BG <s5460703@gmail.com>
2024-05-03 04:24:28 +0800

    ✨ feat：新增 README 页面

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import createMDX from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -15,6 +17,11 @@ const nextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
--- src/common/components/layouts/index.tsx
+++ src/common/components/layouts/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
 
 import HeaderTop from './header/HeaderTop';
@@ -9,17 +10,22 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const pathname = usePathname();
   const { resolvedTheme, setTheme } = useTheme();
   setTheme(resolvedTheme!);
 
   return (
     <>
-      <div className="relative flex h-dvh flex-col">
-        <HeaderTop />
-        <main className="absolute top-[10vh] w-full flex-1 transition-all duration-300">
-          {children}
-        </main>
-      </div>
+      {pathname === '/readme' ? (
+        <>{children}</>
+      ) : (
+        <div className="relative flex h-dvh flex-col">
+          <HeaderTop />
+          <main className="absolute top-[10vh] w-full flex-1 transition-all duration-300">
+            {children}
+          </main>
+        </div>
+      )}
     </>
   );
 };
