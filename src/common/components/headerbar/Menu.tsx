8e32c33d257d3ea676aa3ac982f157de510f7ee8
墨綠BG <s5460703@gmail.com>
2024-05-03 20:07:07 +0800

    🌈 style：代码格式化

--- src/common/components/headerbar/Menu.tsx
+++ src/common/components/headerbar/Menu.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/common/components/ui/button';
 
-import MenuItem from './MenuItem';
 import { ModeToggle } from '../elements/ModeToggle';
+import MenuItem from './MenuItem';
 
 const Menu_ITEMS = [
   { href: '/', value: '演示' },
@@ -20,7 +20,7 @@ const Menu: React.FC = () => {
         <Button className="mx-[5%] rounded-none bg-black text-[1.6vh] text-white hover:bg-gray-800 dark:bg-gray-700 hover:dark:bg-gray-600">
           开始体验
         </Button>
-        <div className='mr-2'>
+        <div className="mr-2">
           <ModeToggle />
         </div>
       </ul>
--- src/common/libs/mdx-componunts.tsx
+++ src/common/libs/mdx-componunts.tsx
@@ -2,9 +2,7 @@ import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    a: ({ children }) => (
-      <a className='text-blue-800 underline'>{children}</a>
-    ),
+    a: ({ children }) => <a className="text-blue-800 underline">{children}</a>,
     ...components,
   };
 }
