692a8d1497fb73882bc1fbaccddc4414874c5214
墨綠BG <s5460703@gmail.com>
2024-05-04 05:37:22 +0800

    🐞 fix：修正导航栏中文换行问题

--- src/common/components/headerbar/Menu.tsx
+++ src/common/components/headerbar/Menu.tsx
@@ -8,23 +8,26 @@ const Menu_ITEMS = [
   { href: '/api', value: '资源' },
   { href: '/examples', value: '用例' },
   { href: '/readme', value: 'README' },
+  { href: '/robot', value: '机器人' },
 ];
 
 const Menu: React.FC = () => {
   return (
-    <nav className="absolute right-[10px] flex h-full w-auto items-center">
-      <ul className="flex h-full items-center">
-        {Menu_ITEMS.map((item, index) => (
-          <MenuItem key={index} href={item.href} value={item.value} />
-        ))}
-        <Button className="mx-[5%] rounded-none bg-black text-[1.6vh] text-white hover:bg-gray-800 dark:bg-gray-700 hover:dark:bg-gray-600">
-          开始体验
-        </Button>
-        <div className="mr-2">
-          <ModeToggle />
-        </div>
-      </ul>
-    </nav>
+    <div className="absolute right-[30px] flex h-full w-auto items-center">
+      <nav className="relative right-[100px] flex h-full items-center">
+        <ul className="flex h-full items-center">
+          {Menu_ITEMS.map((item, index) => (
+            <MenuItem key={index} href={item.href} value={item.value} />
+          ))}
+        </ul>
+      </nav>
+      <Button className="mx-[5%] rounded-none bg-black text-[1.6vh] text-white hover:bg-gray-800 dark:bg-gray-700 hover:dark:bg-gray-600">
+        开始体验
+      </Button>
+      <div className="mr-2">
+        <ModeToggle />
+      </div>
+    </div>
   );
 };
 
