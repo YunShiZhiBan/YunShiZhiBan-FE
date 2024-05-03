e5cc7e79972cc37157fb9d91535ae9d2c47f7ff2
墨綠BG <s5460703@gmail.com>
2024-05-03 20:56:58 +0800

    ✨ feat：新增机器人助理

---
+++ src/modules/home/components/MainTabs/components/ChatBot.tsx
@@ -0,0 +1,13 @@
+const ChatBot: React.FC = () => {
+  return (
+    <iframe
+      src="https://www.chatbase.co/chatbot-iframe/637zQZNRXyVYZTiVKTwQe"
+      title="Chatbot"
+      width="100%"
+      className="h-full min-h-[700px]"
+      frameBorder="0"
+    ></iframe>
+  );
+};
+
+export default ChatBot;
