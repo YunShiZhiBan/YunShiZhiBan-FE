ef28a7afd9839e5852cf3db037a931d7036dde5b
墨綠BG <s5460703@gmail.com>
2024-05-04 13:19:44 +0800

    ✨ feat：新增资源文件

--- src/modules/home/components/Resource/components/Resource.tsx
+++ src/modules/home/components/Resource/components/Resource.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-
 type ResourceProps = {};
 
 const Resource: React.FC<ResourceProps> = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <div>
+      <p className="text-2xl">人物 character</p>
+      <p className="text-2xl">背景 background</p>
+      <p className="text-2xl">PPT PowerPoint</p>
+      <p className="text-2xl">音频 audio</p>
+    </div>
+  );
 };
 
 export default Resource;
