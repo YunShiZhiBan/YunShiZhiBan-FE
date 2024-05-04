2cab77f61287d37141023bed6fba59b2adeef9d9
墨綠BG <s5460703@gmail.com>
2024-05-04 15:37:38 +0800

    🔨 refactor：添加 Accordion 组件

--- src/modules/home/components/Resource/components/Resource.tsx
+++ src/modules/home/components/Resource/components/Resource.tsx
@@ -1,19 +1,50 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/common/components/ui/accordion';
+
 import AudioSource from './AudioSource';
 import ImageSource from './ImageSource';
 import PPTSource from './PPTSource';
 
 const Resource: React.FC = () => {
   return (
-    <div>
-      <p className="text-2xl underline">人物 character</p>
-      <ImageSource type="CharacterImages" />
-      <p className="text-2xl underline">背景 background</p>
-      <ImageSource type="BackgroundImages" />
-      <p className="text-2xl underline">PPT PowerPoint</p>
-      <PPTSource />
-      <p className="text-2xl underline">音频 audio</p>
-      <AudioSource />
-    </div>
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>
+          <p className="text-2xl">人物 character</p>
+        </AccordionTrigger>
+        <AccordionContent>
+          <ImageSource type="CharacterImages" />
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>
+          <p className="text-2xl">背景 background</p>
+        </AccordionTrigger>
+        <AccordionContent>
+          <ImageSource type="BackgroundImages" />
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>
+          <p className="text-2xl">PPT PowerPoint</p>
+        </AccordionTrigger>
+        <AccordionContent>
+          <PPTSource />
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>
+          <p className="text-2xl">音频 audio</p>
+        </AccordionTrigger>
+        <AccordionContent>
+          <AudioSource />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
