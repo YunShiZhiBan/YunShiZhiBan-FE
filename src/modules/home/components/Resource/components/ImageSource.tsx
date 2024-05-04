f4c06e2310279400782a5bda12e1ed7a2552b380
墨綠BG <s5460703@gmail.com>
2024-05-04 15:22:24 +0800

    🔨 refactor：图片懒加载

--- src/modules/home/components/Resource/components/ImageSource.tsx
+++ src/modules/home/components/Resource/components/ImageSource.tsx
@@ -5,23 +5,23 @@ interface ImageProps {
 }
 
 const ImageSource: React.FC<ImageProps> = ({ type }) => {
-  const CharacterImages = ['001.png', '002.png','003.png','004.png'];
-  const BackgroundImages = ['001.jpg', '002.jpg','003.png','004.jpg'];
+  const CharacterImages = ['001.png', '002.png', '003.png', '004.png'];
+  const BackgroundImages = ['001.jpg', '002.jpg', '003.png', '004.jpg'];
 
   return (
     <div className="flex gap-2 py-2">
       {type === 'CharacterImages' &&
-        CharacterImages.map(
-          (image, index) =>
-            index < 2 ? (
-              <Image
-                key={index}
-                src={`/images/character/${image}`}
-                alt={`Character ${index}`}
-                width={200}
-                height={200}
-              />
-            ):
+        CharacterImages.map((image, index) =>
+          index < 2 ? (
+            <Image
+              key={index}
+              src={`/images/character/${image}`}
+              alt={`Character ${index}`}
+              width={200}
+              height={200}
+              loading="lazy"
+            />
+          ) : (
             index > 1 && (
               <Image
                 key={index}
@@ -29,8 +29,10 @@ const ImageSource: React.FC<ImageProps> = ({ type }) => {
                 alt={`Character ${index}`}
                 width={300}
                 height={200}
+                loading="lazy"
               />
             )
+          )
         )}
       {type === 'BackgroundImages' &&
         BackgroundImages.map(
@@ -42,6 +44,7 @@ const ImageSource: React.FC<ImageProps> = ({ type }) => {
                 alt={`Background ${index}`}
                 width={200}
                 height={200}
+                loading="lazy"
               />
             )
         )}
