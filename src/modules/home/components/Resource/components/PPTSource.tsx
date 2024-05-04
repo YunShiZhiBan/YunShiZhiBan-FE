2152f03add5cab58b5b52e980fd7e4d73492ad67
墨綠BG <s5460703@gmail.com>
2024-05-04 15:27:10 +0800

    ✨ feat：更新 ppt 加载数量

--- src/modules/home/components/Resource/components/PPTSource.tsx
+++ src/modules/home/components/Resource/components/PPTSource.tsx
@@ -5,7 +5,24 @@ import { FileText } from 'lucide-react';
 import { Button } from '@/common/components/ui/button';
 
 const PPTSource: React.FC = () => {
-  const Files = ['document (1).pdf','document (2).pdf','document (3).pdf'];
+  const Files = [
+    'document.pdf',
+    'document (1).pdf',
+    'document (2).pdf',
+    'document (3).pdf',
+    'document (4).pdf',
+    'document (5).pdf',
+    'document (6).pdf',
+    'document (7).pdf',
+    'document (8).pdf',
+    'document (9).pdf',
+    'document (10).pdf',
+    'document (11).pdf',
+    'document (12).pdf',
+    'document (13).pdf',
+    'document (14).pdf',
+    'document (15).pdf',
+  ];
 
   const handleOpenPdf = (file: string) => {
     const pdfUrl = `/powerpoint/${file}`;
